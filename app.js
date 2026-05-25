1542f6ba624c2fa27d8c87709fabd1b362278c83
jontive21 <joseeontiveros@sanluis.edu.ar>
2026-05-24 21:10:20 -0300

    feat: servir archivos estáticos desde la carpeta public

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ app.set('views', path.join(__dirname, 'views'));
 // Middlewares
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+// Servidor de archivos estáticos (para CSS, imágenes, etc.)
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Rutas
 const fotazaRoutes = require('./routes/fotazaRoutes');
