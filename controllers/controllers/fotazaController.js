c74bfce4c7cf999d6161a4f22bb7f843dd9699db
jontive21 <joseeontiveros@sanluis.edu.ar>
2026-05-24 22:39:18 -0300

    Verificando funcionamiento de listado fotos

---
+++ controllers/controllers/fotazaController.js
@@ -0,0 +1,27 @@
+const { Fotaza } = require('../models');
+
+const fotazaController = {
+    // Método para listar todas las fotos
+    index: async (req, res) => {
+        try {
+            const fotos = await Fotaza.findAll();
+            // Esto buscará una vista llamada 'listaFotazas.pug'
+            res.render('listaFotazas', { fotos: fotos });
+        } catch (error) {
+            console.error(error);
+            res.status(500).send("Error al obtener las fotos de la base de datos");
+        }
+    },
+
+    // Método para crear una nueva foto (lo usaremos luego en el formulario)
+    create: async (req, res) => {
+        try {
+            await Fotaza.create(req.body);
+            res.redirect('/fotazas');
+        } catch (error) {
+            res.status(500).send("Error al guardar la foto");
+        }
+    }
+};
+
+module.exports = fotazaController;
