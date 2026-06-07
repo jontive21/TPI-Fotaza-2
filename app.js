a1b5321f2371e10ae556414f2568ef76a16938cd
jontive21 <joseeontiveros@sanluis.edu.ar>
2026-06-07 18:58:24 -0300

    Fix: forzar recreación de tablas

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ const sequelize = db.sequelize;
 sequelize.authenticate()
   .then(() => {
     console.log('✅ Conexión a la base de datos establecida.');
-    return sequelize.sync();
+    return sequelize.sync({ force: true });
   })
   .then(() => {
     console.log('✅ Tablas sincronizadas.');
