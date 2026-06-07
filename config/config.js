5c59ef3cbf570d3217543c01554211cd30eca6d8
jontive21 <joseeontiveros@sanluis.edu.ar>
2026-06-07 15:09:08 -0300

    Fix de configuración de host para producción

--- config/config.js
+++ config/config.js
@@ -1,26 +1,20 @@
-require('dotenv').config();
+﻿require('dotenv').config();
+
 module.exports = {
   development: {
     username: process.env.DB_USERNAME || "root",
     password: process.env.DB_PASSWORD || null,
     database: process.env.DB_NAME || "fotaza_db",
     host: process.env.DB_HOST || "127.0.0.1",
-    dialect: process.env.DB_DIALECT || "mysql"
-  },
-  test: {
-    username: process.env.DB_USERNAME || "root",
-    password: process.env.DB_PASSWORD || null,
-    database: "fotaza_db_test",
-    host: process.env.DB_HOST || "127.0.0.1",
-    dialect: process.env.DB_DIALECT || "mysql"
+    dialect: "mysql"
   },
   production: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: 'mysql',
+    port: process.env.DB_PORT || 4000,
+    dialect: "mysql",
     dialectOptions: {
       ssl: {
         require: true,
